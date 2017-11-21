e80f3988fb38615a1ad388a36d7ed277bb636abd
William Rossi <William.Rossi@iam.local>
2017-11-21 16:27:27 -0600

    First Push

--- Poker_Game/MainWindow.xaml.cs
+++ Poker_Game/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Poker_Game
 {
@@ -31,6 +21,9 @@ namespace Poker_Game
         private void Draw_Click(object sender, RoutedEventArgs e)
         {
             deck.PlayerDraw();
+            PlayerTotal.Text = Convert.ToString(PlayerTotal);
+            deck.DealerDraw();
+            DealerTotal.Text = Convert.ToString(DealerTotal);
         }//Draws a card for the player
 
         private void PlayerTotal_TextInput(object sender, TextCompositionEventArgs e)
